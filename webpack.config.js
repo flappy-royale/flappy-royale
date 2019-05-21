01c167f1bdb8df56d5bbfa1b8033d83551ec807e
Mike Lazer-Walker <michael@lazerwalker.com>
2019-05-20 22:06:08 -0400

    Configure Webpack and gh-pages to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/app.ts',
@@ -8,15 +11,31 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
-      }
+      },
     ]
   },
   resolve: {
-    extensions: [ '.ts', '.tsx', '.js' ]
+    extensions: ['.ts', '.tsx', '.js'],
   },
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist')
   },
-  mode: 'development'
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Flappy Royale"
+    }),
+    new HtmlWebpackTagsPlugin({ tags: ['style.css'], append: true }),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'style.css'),
+        to: path.resolve(__dirname, 'dist')
+      },
+      {
+        from: path.resolve(__dirname, 'assets', '**', '*'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ]),
+  ]
 };
