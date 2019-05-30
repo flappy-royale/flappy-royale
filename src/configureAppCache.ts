434a4aa5be89a5f09eacfc2037423cd8f53f5e6a
Mike Lazer-Walker <michael@lazerwalker.com>
2019-05-30 11:07:03 +0200

    Add logging around app cache events

--- src/configureAppCache.ts
+++ src/configureAppCache.ts
@@ -1,14 +1,35 @@
 export default () => {
-  // Check if a new cache is available on page load.
-  // (Copy/pasted from https://www.html5rocks.com/en/tutorials/appcache/beginner/)
   window.addEventListener('load', function (e) {
-    window.applicationCache.addEventListener('updateready', function (e) {
+    window.applicationCache.addEventListener('cached', (e) => {
+      console.log("AppCache: 'cached' event", e)
+    }, false)
+
+    window.applicationCache.addEventListener('checking', (e) => {
+      console.log("AppCache: 'checking' event", e)
+    }, false)
+
+    window.applicationCache.addEventListener('error', (e) => {
+      console.log("AppCache: 'error' event", e)
+    }, false)
+
+    window.applicationCache.addEventListener('noupdate', (e) => {
+      console.log("AppCache: 'noupdate' event", e)
+    }, false)
+
+    window.applicationCache.addEventListener('obsolete', (e) => {
+      console.log("AppCache: 'cached' event", e)
+    }, false)
+
+    window.applicationCache.addEventListener('downloading', (e) => {
+      console.log("Downloading a new version of the app!", e)
+    }, false)
+
+    window.applicationCache.addEventListener('updateready', (e) => {
       if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
         // A new version of the app is ready, reload it
         // TODO: Do this automatically! For now, though, I want to test if this works
-        if (confirm('A new version of this site is available. Load it?')) {
-          window.location.reload();
-        }
+        alert("A new version of this site is available. Let's load it!")
+        window.location.reload();
       }
     }, false);
   }, false);
