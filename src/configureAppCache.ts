9ac4c03cb720d439704c53aadadcd24948819a57
Mike Lazer-Walker <michael@lazerwalker.com>
2019-05-30 11:23:13 +0200

    Add hooks in place to build a nice AppCache loading screen

--- src/configureAppCache.ts
+++ src/configureAppCache.ts
@@ -1,36 +1,27 @@
 export default () => {
   window.addEventListener('load', function (e) {
-    window.applicationCache.addEventListener('cached', (e) => {
-      console.log("AppCache: 'cached' event", e)
-    }, false)
-
-    window.applicationCache.addEventListener('checking', (e) => {
-      console.log("AppCache: 'checking' event", e)
-    }, false)
+    /* There are some appCache events we're not currently listening to:
+     * - 'cached': I think this happens on initial first-time caching, but I can't get it to trigger?
+     * - 'checking': Fires more or less on pageload
+     * - 'noupdate': We've confirmed there's no new version to download
+     * - 'obsolete': ???
+     * */
 
     window.applicationCache.addEventListener('error', (e) => {
-      console.log("AppCache: 'error' event", e)
-    }, false)
-
-    window.applicationCache.addEventListener('noupdate', (e) => {
-      console.log("AppCache: 'noupdate' event", e)
-    }, false)
-
-    window.applicationCache.addEventListener('obsolete', (e) => {
-      console.log("AppCache: 'cached' event", e)
+      console.log("AppCache error", e)
     }, false)
 
     window.applicationCache.addEventListener('downloading', (e) => {
       console.log("Downloading a new version of the app!", e)
     }, false)
 
+    window.applicationCache.addEventListener('progress', (e: ProgressEvent) => {
+      console.log(`Downloading new version: ${e.loaded}/${e.total} = ${Math.floor(100 * (e.loaded / e.total))}%`)
+    })
+
     window.applicationCache.addEventListener('updateready', (e) => {
-      if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-        // A new version of the app is ready, reload it
-        // TODO: Do this automatically! For now, though, I want to test if this works
-        alert("A new version of this site is available. Let's load it!")
-        window.location.reload();
-      }
+      alert("A new version of the game is available! We need to reload the page to update.")
+      window.location.reload()
     }, false);
   }, false);
 }
