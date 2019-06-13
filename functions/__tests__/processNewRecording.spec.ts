5ccc74d3390f07f6dca58801d9ec3cbeb8023467
Mike Lazer-Walker <michael@lazerwalker.com>
2019-06-13 19:54:37 +0200

    Flesh out high score tests to make them slightly more meaningful without more specs

--- functions/__tests__/processNewRecording.spec.ts
+++ functions/__tests__/processNewRecording.spec.ts
@@ -18,19 +18,21 @@ describe("processNewRecording", () => {
       it("should overwrite a lower score", () => {
         const lower = PlayerDataFactory({ score: 1 })
         const higher = PlayerDataFactory({ score: 5 })
+        const other = PlayerDataFactory({ score: 3 }, "Orta")
 
-        const start = { replays: [lower] }
+        const start = { replays: [lower, other] }
         const result = processNewRecording(start, higher, lower.user.name, 2)
-        expect(result).toEqual({ replays: [higher] })
+        expect(result).toEqual({ replays: [higher, other] })
       })
 
       it("should be ignored if the earlier score is higher", () => {
         const lower = PlayerDataFactory({ score: 1 })
         const higher = PlayerDataFactory({ score: 5 })
+        const other = PlayerDataFactory({ score: 3 }, "Orta")
 
-        const start = { replays: [higher] }
+        const start = { replays: [higher, other] }
         const result = processNewRecording(start, lower, lower.user.name, 2)
-        expect(result).toEqual({ replays: [higher] })
+        expect(result).toEqual({ replays: [higher, other] })
       })
     })
   })
