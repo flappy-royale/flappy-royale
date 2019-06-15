89f60e375dbdb67cea8568069ff3bf7560a9127a
Orta Therox <orta.therox@gmail.com>
2019-06-15 14:30:18 -0700

    Update trial icon - fixes #83

--- src/menus/MainMenuScene.ts
+++ src/menus/MainMenuScene.ts
@@ -84,7 +84,7 @@ export class MainMenuScene extends Phaser.Scene {
         const royaleButton = this.add.image(84, 110 + c.NotchOffset, "royale-button")
         becomeButton(royaleButton, this.loadRoyale, this)
 
-        const trial = this.add.image(74, 146 + c.NotchOffset, "trial-button")
+        const trial = this.add.image(74, 152 + c.NotchOffset, "trial-button")
         becomeButton(trial, this.loadTrial, this)
 
         const settingsButton = this.add.image(76, 200 + c.NotchOffset, "settings-button")
