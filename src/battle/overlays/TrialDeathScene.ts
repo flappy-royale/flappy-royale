fa0d7cdf07b5b0a896ea0009b25b037361ff3343
Mike Lazer-Walker <michael@lazerwalker.com>
2019-06-17 15:23:03 +0200

    More renaming trial -> daily trial

--- src/battle/overlays/TrialDeathScene.ts
+++ src/battle/overlays/TrialDeathScene.ts
@@ -182,7 +182,7 @@ export class TrialDeath extends Phaser.Scene {
         this.add.image(20, GameHeight - 48, "green-sash")
         const userScores = getUserStatistics()
         const pos = getNumberWithOrdinal(userScores.trialSpecificWins)
-        this.add.bitmapText(8, GameHeight - 56, "fipps-bit", `${pos} trial win`, 8)
+        this.add.bitmapText(8, GameHeight - 56, "fipps-bit", `${pos} daily trial win`, 8)
     }
 
     private cameInTopThree(player: PlayerData, sortedReplays: PlayerData[]) {
