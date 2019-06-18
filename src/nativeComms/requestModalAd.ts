b710b59d19d7a4e2a37b824e3bcf739e834b8951
Orta Therox <orta.therox@gmail.com>
2019-06-17 21:15:16 -0700

    Adds a note aboute watching ads

--- src/nativeComms/requestModalAd.ts
+++ src/nativeComms/requestModalAd.ts
@@ -1,6 +1,9 @@
 import { LifeStateForSeed } from "../user/userManager"
+import { analyticsEvent } from "./analytics"
 
 export function requestModalAd(current: LifeStateForSeed) {
+    analyticsEvent("watching_ad", { state: current })
+
     if (window.isAppleApp) {
         window.webkit.messageHandlers.adManager.postMessage(current)
     } else {
