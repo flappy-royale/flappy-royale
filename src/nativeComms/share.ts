725a124291d2c25c6fd05f3b332ed669ab004080
Orta Therox <orta.therox@gmail.com>
2019-06-17 21:13:58 -0700

    Adds an analytics event for share

--- src/nativeComms/share.ts
+++ src/nativeComms/share.ts
@@ -1,17 +1,27 @@
+import { analyticsEvent } from "./analytics"
+
 interface ScreenshotableScreen extends Phaser.Scene {
     showScreenshotUI: () => void
     removeScreenshotUI: () => void
 }
 
 export function shareNatively(copy: string, scene: ScreenshotableScreen) {
+    analyticsEvent("share", {})
+
     if (window.isAppleApp) {
         scene.showScreenshotUI()
 
-        setTimeout(() => { window.webkit.messageHandlers.shareScreenshot.postMessage(copy) }, 100)
+        setTimeout(() => {
+            window.webkit.messageHandlers.shareScreenshot.postMessage(copy)
+        }, 100)
 
-        window.addEventListener("screenshotComplete", () => {
-            scene.removeScreenshotUI()
-        }, { once: true })
+        window.addEventListener(
+            "screenshotComplete",
+            () => {
+                scene.removeScreenshotUI()
+            },
+            { once: true }
+        )
     } else {
         if (navigator && "share" in navigator) {
             const n = navigator as any
