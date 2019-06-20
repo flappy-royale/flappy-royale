c12d94fbde087b616ca6ec9597123d74e7954aad
Mike Lazer-Walker <michael@lazerwalker.com>
2019-06-20 12:29:42 +0200

    Strip out unneeded(?) ad config [#93]

--- android/app/src/main/java/com/example/flappyroyale/MainActivity.kt
+++ android/app/src/main/java/com/example/flappyroyale/MainActivity.kt
@@ -13,16 +13,6 @@ import com.mopub.common.SdkInitializationListener
 import com.mopub.common.logging.MoPubLog
 import com.mopub.mobileads.MoPubView
 
-import com.applovin.sdk.AppLovinSdk
-
-import com.vungle.warren.InitCallback
-import com.vungle.warren.Vungle
-
-import com.chartboost.sdk.Chartboost;
-import com.chartboost.sdk.CBLocation;
-import com.chartboost.sdk.ChartboostDelegate;
-import com.mopub.mobileads.GooglePlayServicesAdapterConfiguration
-
 class MainActivity : AppCompatActivity() {
     private var moPubView: MoPubView? = null
 
@@ -38,7 +28,6 @@ class MainActivity : AppCompatActivity() {
         webview.loadUrl("https://flappyroyale.io/prod")
 
         // TODO: setUpFirebase()
-        setUpVungle()
         setUpMoPub()
 
         /** Set up the bottom banner ad */
@@ -49,45 +38,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun setUpVungle() {
-        val vungleCallback = object : InitCallback {
-            override fun onSuccess() {
-                // Initialization has succeeded and SDK is ready to load an ad or play one if there
-                // is one pre-cached already
-            }
-
-            override fun onError(throwable: Throwable) {
-                // Initialization error occurred - throwable.getLocalizedMessage() contains error message
-            }
-
-            override fun onAutoCacheAdAvailable(placementId: String) {
-                // Callback to notify when an ad becomes available for the auto-cached placement
-                // NOTE: This callback works only for the auto-cached placement. Otherwise, please use
-                // LoadAdCallback with loadAd API for loading placements.
-            }
-        }
-        Vungle.init(AdConstants.vungleAppID, getApplicationContext(), vungleCallback)
-    }
-
     fun setUpMoPub() {
-        val admobConfig = HashMap<String,String>() //define empty hashmap
-        admobConfig.put("npa", "1");
-
         val moPubConfig = SdkConfiguration.Builder(AdConstants.bottomBannerMoPub)
             .withLogLevel(MoPubLog.LogLevel.DEBUG)
-            .withMediatedNetworkConfiguration(GooglePlayServicesAdapterConfiguration::class.java.name, admobConfig)
             .build()
 
 
         val moPubCompletion = object : SdkInitializationListener {
-            override fun onInitializationFinished() {
-                // Not sure why these are here, but this is our exact flow on iOS
-
-                AppLovinSdk.initializeSdk(applicationContext);
-
-                // We need access to the current Activity, but within this listener "this" isn't that!
-                setUpChartboost()
-            }
+            override fun onInitializationFinished() {}
         }
 
         MoPub.initializeSdk(
@@ -95,11 +53,6 @@ class MainActivity : AppCompatActivity() {
             moPubConfig,
             moPubCompletion
         )
-    }
-
-    fun setUpChartboost() {
-        Chartboost.startWithAppId(this, AdConstants.chartboostAppId, AdConstants.chartboostAppSignature);
-        Chartboost.onCreate(this);
     }
 
     override fun onDestroy() {
