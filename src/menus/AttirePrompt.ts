51467faa258594f22f898ba96d1d4274caeb1b7b
Mike Lazer-Walker <michael@lazerwalker.com>
2019-06-25 13:29:38 +0200

    More name/attire prompt styling [#108]

--- src/menus/AttirePrompt.ts
+++ src/menus/AttirePrompt.ts
@@ -1,6 +1,6 @@
 import * as Phaser from "phaser"
 import _ = require("lodash")
-import { GameWidth, GameHeight } from "../constants"
+import { GameWidth, GameHeight, zLevels } from "../constants"
 import { changeSettings } from "../user/userManager"
 import { becomeButton } from "./utils/becomeButton"
 import { rightAlignTextLabel, centerAlignTextLabel } from "../battle/utils/alignTextLabel"
@@ -18,6 +18,7 @@ export class AttirePrompt extends Phaser.Scene {
     preload() {
         this.load.image("attire-button-small-bg", require("../../assets/menu/ButtonSmallBG.png"))
         this.load.image("attire-button-small-bg-light", require("../../assets/menu/ButtonSmallBG-Light.png"))
+        this.load.image("attire-button-small-bg-yellow", require("../../assets/menu/ButtonSmallBG-Yellow.png"))
         this.load.bitmapFont("fipps", require("../../assets/fonts/fipps.png"), require("../../assets/fonts/fipps.fnt"))
     }
 
@@ -28,14 +29,26 @@ export class AttirePrompt extends Phaser.Scene {
         centerAlignTextLabel(title)
         centerAlignTextLabel(subtitle)
 
+        title.setDepth(zLevels.ui)
+        subtitle.setDepth(zLevels.ui)
+
         const noBg = this.add.image(GameWidth / 3, subtitle.y + 40, "attire-button-small-bg")
+        noBg.setDepth(zLevels.ui)
         const noText = this.add.bitmapText(noBg.x - 20, noBg.y - 5, "fipps", "LATER", 8)
+        noText.setDepth(zLevels.ui)
         becomeButton(noBg, this.no, this, [noText])
 
-        const yesBg = this.add.image((4 * GameWidth) / 5, subtitle.y + 40, "attire-button-small-bg-light")
+        const yesBg = this.add.image((4 * GameWidth) / 5, subtitle.y + 40, "attire-button-small-bg-yellow")
+        yesBg.setDepth(zLevels.ui)
         yesBg.x = GameWidth - yesBg.width
         const yesText = this.add.bitmapText(yesBg.x - 20, yesBg.y - 5, "fipps", "YEAH!", 8)
         becomeButton(yesBg, this.yes, this, [yesText])
+        yesText.setDepth(zLevels.ui)
+
+        const height = yesBg.y + yesBg.height - title.y + 10
+        const y = title.y - 10 + height / 2
+        const bg = this.add.rectangle(GameWidth / 2, y, GameWidth, height, 0x8991eb)
+        bg.setDepth(zLevels.uiBg)
     }
 
     private yes() {
