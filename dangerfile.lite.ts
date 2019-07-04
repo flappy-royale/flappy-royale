aed4777d341b6e39851aa056718d52d75654f43f
Orta Therox <orta.therox@gmail.com>
2019-07-04 09:40:24 -0400

    Update the script

--- dangerfile.lite.ts
+++ dangerfile.lite.ts
@@ -11,8 +11,10 @@ if (!appTS.includes("const startupScreen = StartupScreen.MainMenu")) {
 // Ensures we ship submodules
 const subm = execSync("git submodule status", { encoding: "utf8" }).split(" ")
 if (subm.length === 4) {
-    const sha = subm[0]
-    const check = spawnSync(`git --git-dir ./assets/.git --work-tree=./assets/ branch origin/master --contains ${sha}`)
+    const sha = subm[1]
+    const cmd = `git --git-dir ./assets/.git --work-tree=./assets/ branch origin/master --contains ${sha}`
+    console.log(cmd)
+    const check = spawnSync(cmd, { encoding: "utf8" })
     if (check.status) {
         throw "You need to push your assets to github"
     }
