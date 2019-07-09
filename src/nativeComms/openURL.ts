a6c0ce04ee0374983f467f096a24e5bb6e601b7f
Orta Therox <orta.therox@gmail.com>
2019-07-09 09:23:22 -0400

    Classes all android funcs as optional, and forces you to jump through hoops to propve they exist first

--- src/nativeComms/openURL.ts
+++ src/nativeComms/openURL.ts
@@ -2,7 +2,7 @@
 export function openURL(url: string) {
     if (window.isAppleApp) {
         window.webkit.messageHandlers.openURL.postMessage(url)
-    } else if (window.URLLoader) {
+    } else if (window.URLLoader && window.URLLoader.openURL) {
         window.URLLoader.openURL(url)
     } else {
         window.open(url, "_blank")
