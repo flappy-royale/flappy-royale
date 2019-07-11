b46e6a3330da87bf9a3b4788823d77158bd91458
Orta Therox <orta.therox@gmail.com>
2019-07-10 21:49:43 -0400

    Minor refacrot

--- src/user/canWearAttire.ts
+++ src/user/canWearAttire.ts
@@ -1,8 +1,8 @@
 import { PresentationAttire } from "../attire"
 import { UserSettings } from "./userManager"
-import * as _ from "lodash"
+import { includes } from "lodash"
 
 // This can be fairly naive because we'll verify actual ownership on the server.
 export const canWearAttire = (settings: UserSettings, attire: PresentationAttire): boolean => {
-    return attire.free || _.includes(settings.unlockedAttire, attire.id)
+    return attire.free || includes(settings.unlockedAttire, attire.id)
 }
