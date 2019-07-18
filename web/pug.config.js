0794be76668645cd9dbb708eeaf2cb2d32a546f2
Orta Therox <orta.therox@gmail.com>
2019-07-18 09:59:28 -0400

    Adds a news section to the site

---
+++ web/pug.config.js
@@ -0,0 +1,15 @@
+const { readdirSync, readFileSync } = require("fs")
+const { join } = require("path")
+
+var MarkdownIt = require("markdown-it")
+const md = new MarkdownIt()
+
+const postPaths = readdirSync(join(__dirname, "blog"), "utf8").map(p => `${__dirname}/blog/${p}`)
+const posts = postPaths.map(p => readFileSync(p, "utf8")).map(content => md.render(content))
+
+module.exports = {
+    locals: {
+        hello: "world",
+        posts: posts
+    }
+}
