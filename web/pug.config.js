79d0553f2adcde906e749cc57ae578acaab42d51
Orta Therox <orta.therox@gmail.com>
2019-08-11 18:54:03 -0400

    Reverse the order for posts

--- web/pug.config.js
+++ web/pug.config.js
@@ -4,7 +4,9 @@ const { join } = require("path")
 var MarkdownIt = require("markdown-it")
 const md = new MarkdownIt()
 
-const postPaths = readdirSync(join(__dirname, "blog"), "utf8").map(p => `${__dirname}/blog/${p}`)
+const postPaths = readdirSync(join(__dirname, "blog"), "utf8")
+    .map(p => `${__dirname}/blog/${p}`)
+    .reverse()
 const posts = postPaths.map(p => readFileSync(p, "utf8")).map(content => md.render(content))
 
 module.exports = {
